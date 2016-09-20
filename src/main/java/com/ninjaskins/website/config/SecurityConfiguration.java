d3bdc67cf181a6645bb56b82d343feac964bd0be
marcus <ms2asdk@gmail.com>
2016-09-20 04:20:48 -0700

    permit /jackpot deposits

--- src/main/java/com/ninjaskins/website/config/SecurityConfiguration.java
+++ src/main/java/com/ninjaskins/website/config/SecurityConfiguration.java
@@ -113,6 +113,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset_password/init").permitAll()
             .antMatchers("/api/account/reset_password/finish").permitAll()
             .antMatchers("/api/profile-info").permitAll()
+            .antMatchers("/api/jackpot-deposits").permitAll()
             .antMatchers("/api/**").authenticated()
             .antMatchers("/websocket/tracker").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/websocket/**").permitAll()
